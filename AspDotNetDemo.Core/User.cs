647b00c1a4bb8cdef0c5cadd790477509e072f9d
osama kayali <osama1987-l@hotmail.com>
2025-10-01 22:20:19 +0300

    o

---
+++ AspDotNetDemo.Core/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspDotNetDemo.Core
+{
+    internal class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public int Phone { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
