42f97b8d1b528c497096f258468957177fb6e82f
osama kayali <osama1987-l@hotmail.com>
2025-10-03 13:47:09 +0300

    p

--- AspDotNetDemo.data/UserEntity.cs
+++ AspDotNetDemo.data/UserEntity.cs
@@ -8,45 +8,57 @@ namespace AspDotNetDemo.data
     public class UserEntity : IDataHelper<User>
     {
         List<User> ListOfUser;
+        private User user;
         public UserEntity()
         {
             ListOfUser = new List<User>();
-
         }
-        void IDataHelper<User>.Add(User table)
+        public void Add(User table)
         {
-            throw new NotImplementedException();
+            ListOfUser.Add(table);
         }
 
-        void IDataHelper<User>.Delete(int Id)
+        public void Delete(int Id)
         {
-            throw new NotImplementedException();
+            user = Find(Id);
+            ListOfUser.Remove(user);
         }
 
-        void IDataHelper<User>.Edit(int Id, User table)
+        public void Edit(int Id, User table)
         {
-            throw new NotImplementedException();
+            user = Find(Id);
+            user = new User()
+            {
+                Id = table.Id,
+                FirstName = table.FirstName,
+                LastName = table.LastName,
+                Email = table.Email,
+                Password = table.Password,
+                Phone = table.Phone,
+                Bio = table.Bio
+            };
+
         }
 
-        User IDataHelper<User>.Find(int Id)
+        public User Find(int Id)
         {
-            throw new NotImplementedException();
+            return ListOfUser.Where(X => X.Id == Id).First();
         }
 
-        List<User> IDataHelper<User>.GetData()
+        public List<User> GetData()
         {
             return ListOfUser;
         }
 
-        List<User> IDataHelper<User>.Seach(string SearchItem)
+        public List<User> Seach(string SearchItem)
         {
-            return ListOfUser.Where(x => x.Id.ToString() == SearchItem
-            || x.FirstName.Contains(SearchItem)
-            || x.LastName.Contains(SearchItem)
-            || x.Email == SearchItem
-            || x.Bio.Contains(SearchItem)
-            || x.Password.Contains(SearchItem)
-            || x.Phone.ToString() == SearchItem
+            return ListOfUser.Where(X => X.Id.ToString() == SearchItem
+            || X.FirstName.Contains(SearchItem)
+            || X.LastName.Contains(SearchItem)
+            || X.Email == SearchItem
+            || X.Bio.Contains(SearchItem)
+            || X.Password.Contains(SearchItem)
+            || X.Phone.ToString() == SearchItem
             ).ToList();
         }
     }
