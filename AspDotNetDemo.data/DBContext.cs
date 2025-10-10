fad72640d3f37fdf38f0cb4473f70e78ec7309ec
osama kayali <osama1987-l@hotmail.com>
2025-10-10 16:31:51 +0300

    o

---
+++ AspDotNetDemo.data/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using AspDotNetDemo.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspDotNetDemo.data
+{
+    internal class DBContext : DbContext
+    {
+        public DBContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var constring = @"Server=.\SQLEXPRESS;Database=ASPDOTNETDEMO;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(constring);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
